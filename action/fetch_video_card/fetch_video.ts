42a0924f3f5e2b35afd8e4566e2392436eb13816
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-12-24 14:21:45 +0530

    adding https to video url

--- action/fetch_video_card/fetch_video.ts
+++ action/fetch_video_card/fetch_video.ts
@@ -1,7 +1,6 @@
-"use server"
-import {HOST} from "@/utils/configuration/host";
-import {auth} from "@/auth";
-
+"use server";
+import { HOST } from "@/utils/configuration/host";
+import { auth } from "@/auth";
 
 async function getToken() {
     const session = await auth();
@@ -11,27 +10,27 @@ async function getToken() {
 
 export async function fetch_video_card() {
     try {
-
         const token = await getToken();
 
-        console.log(`${HOST.BACKEND_URL}/api/v1/video`, token)
+        console.log(`${HOST.BACKEND_URL}/api/v1/video`, token);
 
-        const res = await fetch(`${HOST.BACKEND_URL}/api/v1/video`
-        )
+        const res = await fetch(
+            `https://blackcat.pratiksomwanshi.online/api/v1/video`
+        );
 
         const data = await res.json();
 
         if (!res.ok) {
-            console.log(data)
+            console.log("data ", res);
             throw new Error(data.error.code || "Failed to fetch video");
         }
 
-
         return data;
-
     } catch (err: any) {
+        console.log("error ", err);
+
         return {
-            error: err.message
-        }
+            error: err.message,
+        };
     }
 }
